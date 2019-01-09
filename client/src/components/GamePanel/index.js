146918b8e5cee899acd155ea92fcb3770beee7da
Ian Dunkelberg <idunkelberg@gmail.com>
2019-01-09 15:16:42 -0800

    clicking shuffles images. Clicking modifies the wrong image's clicked status (this is a logic errror due to using the initial position to modify the altered position. a fix is being developed.)

--- client/src/components/GamePanel/index.js
+++ client/src/components/GamePanel/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import GameCard from "../GameCard";
+//import GameCard from "../GameCard";
 import "./panelStyles.css";
 import Bumblebee from "./bumblebee.png";
 import Devastator from "./devastator.png";
@@ -14,17 +14,20 @@ import Thundercracker from "./thundercracker.png";
 import UltraMagnus from "./ultra_magnus.png";
 import Wheeljack from "./wheeljack.png";
 
+
 class GamePanel extends Component {
   state = {
     score: 0,
     topScore: 0,
     wins: 0,
-    cards: []
+    cards: [],
+    initialCards: []
   };
   componentDidMount = () => {
-    this.setState({cards: this.createCardArray()})
+    this.setState({cards: this.createCardArrays()})
+    
   }
-  createCardArray = () =>{
+  createCardArrays = () =>{
     var arr =[];
     var altArr = ["bumblebee", "devastator","grimlock",
  "megatron","optimus-prime","prowl","shockwave","soundwave","starscream","thundercracker","ultra-magnus","wheeljack"];
@@ -32,22 +35,57 @@ class GamePanel extends Component {
     
     for(var i=0 ; i<12 ; i++){
         var newCard={key:i,
+                    id:i,
                     altText:altArr[i], 
                     clicked:false,
                     href:imgArr[i]}
         arr.push(newCard);
     }
+    console.log(arr);
+    this.setState({initialCards:arr});
     return arr
   }
+  //on lcick we want to 
+    //1) check the clicked status of the clicked on item,
+    //2) if clicked = true  we reset the game
+    //3) if clicked = false update clicked status and shuffle 
   handleClick = (event) =>{
     event.preventDefault();
-    console.log(event);
+    if(this.state.cards[event.target.id].clicked){
+       console.log("Game Over!")
+    }
+    else{
+      console.log( "Id"+event.target.id)
+      var tempArr = this.state.cards;
+      tempArr[event.target.id].clicked = true;
+      console.log(tempArr);
+      this.shuffleArray(tempArr,event.target.id);
+    }
+    
+  }
+  //this handles the re-arranging of elements after click
+  shuffleArray(array,z){
+    console.log("shuffleArray id",z);
+    var i = 0
+    , j = 0
+    , temp = null
+
+    for (i = array.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+    this.setState({cards:array})
   }
   render() {
     return (
       <div className ="game-block">
-          {this.state.cards.map(card => (
-              <GameCard key={card.key} id={card.altText}altText={card.altText} clicked={card.clicked} href={card.href} onClick={this.handleClick}/>
+          { 
+            this.state.cards.map((card) => (
+              <div key = {card.key} className = "picture-card"   onClick={this.handleClick} >
+                <img id={card.id}  className = {card.altText} src={card.href} alt={card.altText}></img>
+              </div>
           ))}
 
       </div>
