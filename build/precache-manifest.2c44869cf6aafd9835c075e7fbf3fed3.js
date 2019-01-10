27a3e3860ed6ae9fa7275686df0f6605de082b6c
Ian Dunkelberg <idunkelberg@gmail.com>
2019-01-10 00:23:19 -0800

    style tweaks implemented(top bar isn't centered and that's fine for now). ready to deploy.

---
+++ build/precache-manifest.2c44869cf6aafd9835c075e7fbf3fed3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "21e0722708c2b3d5511b24cee58ec6ee",
+    "url": "/static/media/optimus_prime.21e07227.png"
+  },
+  {
+    "revision": "d8af4ba392527e10ed56",
+    "url": "/static/css/main.a72f02af.chunk.css"
+  },
+  {
+    "revision": "ebf92a2a080bdfd80192",
+    "url": "/static/js/1.ebf92a2a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5068c84ff488557e7eda4ca4e9965352",
+    "url": "/static/media/bumblebee.5068c84f.png"
+  },
+  {
+    "revision": "adee3d358fe7e2f448e1447a5adf7a3d",
+    "url": "/static/media/devastator.adee3d35.png"
+  },
+  {
+    "revision": "6b23993f5bee7ff9c4aed05c2e554ec6",
+    "url": "/static/media/grimlock.6b23993f.png"
+  },
+  {
+    "revision": "b8b2586ba82019f8e79cef6ad006562d",
+    "url": "/static/media/megatron.b8b2586b.png"
+  },
+  {
+    "revision": "d8af4ba392527e10ed56",
+    "url": "/static/js/main.d8af4ba3.chunk.js"
+  },
+  {
+    "revision": "fe999bdc30765f37bc442785e39025cf",
+    "url": "/static/media/prowl.fe999bdc.png"
+  },
+  {
+    "revision": "9fdcc986eb971f30970221c4eb45c3c7",
+    "url": "/static/media/shockwave.9fdcc986.png"
+  },
+  {
+    "revision": "3f8b396a261ad5090832ca51b152d4fd",
+    "url": "/static/media/soundwave.3f8b396a.png"
+  },
+  {
+    "revision": "db2d3fb415fd0484f8ee5b92be48719a",
+    "url": "/static/media/starscream.db2d3fb4.png"
+  },
+  {
+    "revision": "af83349848372243a56874ae5bbecf3b",
+    "url": "/static/media/thundercracker.af833498.png"
+  },
+  {
+    "revision": "bd338ed602757986b2530b384cb7fcdb",
+    "url": "/static/media/ultra_magnus.bd338ed6.png"
+  },
+  {
+    "revision": "9c59d0b2dcca6ffaf87b26d2e2fb8e6e",
+    "url": "/static/media/wheeljack.9c59d0b2.png"
+  },
+  {
+    "revision": "e70c4570cbd86a5b8a026f1044e0d844",
+    "url": "/index.html"
+  }
+];
