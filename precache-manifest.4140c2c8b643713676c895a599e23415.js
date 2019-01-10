fac248ef163d6abf698f25088b7214a9cac0ee1d
Ian Dunkelberg <idunkelberg@gmail.com>
2019-01-10 00:24:30 -0800

    Updates

---
+++ precache-manifest.4140c2c8b643713676c895a599e23415.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "21e0722708c2b3d5511b24cee58ec6ee",
+    "url": "/react-memory-game/./static/media/optimus_prime.21e07227.png"
+  },
+  {
+    "revision": "47a8c2fb3c3179493387",
+    "url": "/react-memory-game/./static/css/main.a72f02af.chunk.css"
+  },
+  {
+    "revision": "ebf92a2a080bdfd80192",
+    "url": "/react-memory-game/./static/js/1.ebf92a2a.chunk.js"
+  },
+  {
+    "revision": "f84cbdf27a8fcb5f7b2f",
+    "url": "/react-memory-game/./static/js/runtime~main.f84cbdf2.js"
+  },
+  {
+    "revision": "5068c84ff488557e7eda4ca4e9965352",
+    "url": "/react-memory-game/./static/media/bumblebee.5068c84f.png"
+  },
+  {
+    "revision": "adee3d358fe7e2f448e1447a5adf7a3d",
+    "url": "/react-memory-game/./static/media/devastator.adee3d35.png"
+  },
+  {
+    "revision": "6b23993f5bee7ff9c4aed05c2e554ec6",
+    "url": "/react-memory-game/./static/media/grimlock.6b23993f.png"
+  },
+  {
+    "revision": "b8b2586ba82019f8e79cef6ad006562d",
+    "url": "/react-memory-game/./static/media/megatron.b8b2586b.png"
+  },
+  {
+    "revision": "47a8c2fb3c3179493387",
+    "url": "/react-memory-game/./static/js/main.47a8c2fb.chunk.js"
+  },
+  {
+    "revision": "fe999bdc30765f37bc442785e39025cf",
+    "url": "/react-memory-game/./static/media/prowl.fe999bdc.png"
+  },
+  {
+    "revision": "9fdcc986eb971f30970221c4eb45c3c7",
+    "url": "/react-memory-game/./static/media/shockwave.9fdcc986.png"
+  },
+  {
+    "revision": "3f8b396a261ad5090832ca51b152d4fd",
+    "url": "/react-memory-game/./static/media/soundwave.3f8b396a.png"
+  },
+  {
+    "revision": "db2d3fb415fd0484f8ee5b92be48719a",
+    "url": "/react-memory-game/./static/media/starscream.db2d3fb4.png"
+  },
+  {
+    "revision": "af83349848372243a56874ae5bbecf3b",
+    "url": "/react-memory-game/./static/media/thundercracker.af833498.png"
+  },
+  {
+    "revision": "bd338ed602757986b2530b384cb7fcdb",
+    "url": "/react-memory-game/./static/media/ultra_magnus.bd338ed6.png"
+  },
+  {
+    "revision": "9c59d0b2dcca6ffaf87b26d2e2fb8e6e",
+    "url": "/react-memory-game/./static/media/wheeljack.9c59d0b2.png"
+  },
+  {
+    "revision": "86624e57e9fc95ed8f774df1ed3380ca",
+    "url": "/react-memory-game/./index.html"
+  }
+];
