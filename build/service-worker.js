8ef4b301ac0b871120c64973bdeffb907e574ab3
Ian Dunkelberg <idunkelberg@gmail.com>
2019-01-10 00:31:16 -0800

    fixed font styling for navBar.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c44869cf6aafd9835c075e7fbf3fed3.js"
+  "/react-memory-game/./precache-manifest.4140c2c8b643713676c895a599e23415.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-memory-game/./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
